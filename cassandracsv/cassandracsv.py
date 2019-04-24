09b2c35f9f6f1b2423df8bd9b2a79cd8eb698276
William Marquardt <marquardt@navegg.com>
2019-04-23 22:17:02 -0300

    Readme complete

--- cassandracsv/cassandracsv.py
+++ cassandracsv/cassandracsv.py
@@ -1,84 +1,29 @@
-#!/usr/bin/env python
-# coding: UTF-8
+# Cassandra CSV
+Python package to export cassandra query result to CSV format.
+Cassandra `COPY` function does not export data with **WHERE** clause. If you need to export cassandra query result to CSV format, just read the documentation below.
 
-import os
-import re
-import csv
+## Install
+```shell
+    $ pip install cassandra-csv
+```
 
-class CassandraCsv(object):
+## Usage
+```python
+     from cassandracsv import CassandraCsv
+	 result = cassandra_cluster.execute("SELECT foo FROM bar WHERE foobar=2")
+	
+	 CassandraCsv.export(
+		result,
+		[options]
+     )
+```
 
-    filename = ""
-    clean_filename = ""
-    output_dir = ""
-    create_subfolder = False
-
-    def __validate(self, vls):
-        if not os.path.isdir(vls['output_dir']):
-            return (False,"You must set an output directory")
-
-
-        return (True, "ok")
-
-    @classmethod
-    def export(cls, result, max_file_size=0, output_dir=None,
-               filename="output", separator=",", with_header=True,
-               create_subfolder=False):
-
-        validate, reason = me.__validate(locals())
-
-        if not validate:
-            raise Exception(reason)
-
-        # prepare filename.
-        filename = re.sub(r'\.csv$', '', filename)
-        me.clean_filename = filename
-        if max_file_size:
-            filename += """_%(partition)s"""
-        me.file_pool = 0 # set counter to 0 when call export
-        me.max_file_size = max_file_size
-        me.output_dir = output_dir
-        me.filename = filename
-        me.create_subfolder = create_subfolder
-        colnames = result.column_names
-        header = [x.title() for x in colnames]
-        write_list = []
-
-        write_list.append(header)
-        for row in result:
-            write_list.append(list(row))
-            if max_file_size and\
-             len(write_list) >= max_file_size:
-                me.__write(write_list)
-                write_list = [header]
-
-
-        if len(write_list) > 1:
-            me.__write(write_list)
-
-
-    def __write(self, to_file):
-        self.file_pool += 1
-        _path = self.output_dir
-        if self.create_subfolder:
-            _path = os.path.join(_path, self.clean_filename)
-            try:
-                os.mkdir(_path)
-            except:
-                pass
-
-        if self.max_file_size:
-            _file = os.path.join(_path,
-                                 "%s.csv" % (self.filename % {'partition': self.file_pool} ))
-        else:
-            _file = os.path.join(_path,"%s.csv" % self.filename)
-
-        with open(_file, 'w') as csvFile:
-            writer = csv.writer(csvFile)
-            writer.writerows(to_file)
-
-        csvFile.close()
-        return _file
-
-
-# create a class instance
-me = CassandraCsv()
+## Options
+|Name| Type | Default |  Description |
+|--|--|--|--|
+| **max_file_size** | int | 0 | max CSV file size (lines) the file will be splitted until complete entire resultset. Use 0 to create full a file |
+| **output_dir** | string | /tmp | full output path
+| **filename** | string | export_file | file name (without extension)
+| **separator** | string | , | string separator
+| **with_header** | boolean | True | show file header (field name or alias, added direct on select using `as`)
+| **create_subfolder** | boolean | False | create sulfolder to store files.
