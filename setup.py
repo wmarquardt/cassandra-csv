ed190e1e280731faf62ded54ac43dc4db4774da2
William Marquardt <marquardt@navegg.com>
2019-05-28 12:35:41 -0300

    new version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='cassandra-csv',
-    version='0.2',
+    version='0.3',
     packages=find_packages(exclude=['tests*']),
     license='MIT',
     description='Export cassandra query result to CSV',
