bbd4015f6e25faafddce0c9ae0c4674ba7dc9db6
William Marquardt <williammqt@gmail.com>
2023-04-03 09:39:17 -0300

    add black

--- cassandracsv/cassandracsv.py
+++ cassandracsv/cassandracsv.py
@@ -5,36 +5,41 @@ import os
 import re
 import csv
 
-class CassandraCsv(object):
 
+class CassandraCsv(object):
     filename = ""
     clean_filename = ""
     output_dir = ""
     create_subfolder = False
 
     def __validate(self, vls):
-        if not os.path.isdir(vls['output_dir']):
-            return (False,"You must set an output directory")
-
+        if not os.path.isdir(vls["output_dir"]):
+            return (False, "You must set an output directory")
 
         return (True, "ok")
 
     @classmethod
-    def export(cls, result, max_file_size=0, output_dir=None,
-               filename="output", separator=",", with_header=True,
-               create_subfolder=False):
-
+    def export(
+        cls,
+        result,
+        max_file_size=0,
+        output_dir=None,
+        filename="output",
+        separator=",",
+        with_header=True,
+        create_subfolder=False,
+    ):
         validate, reason = me.__validate(locals())
 
         if not validate:
             raise Exception(reason)
 
         # prepare filename.
-        filename = re.sub(r'\.csv$', '', filename)
+        filename = re.sub(r"\.csv$", "", filename)
         me.clean_filename = filename
         if max_file_size:
             filename += """_%(partition)s"""
-        me.file_pool = 0 # set counter to 0 when call export
+        me.file_pool = 0  # set counter to 0 when call export
         me.max_file_size = max_file_size
         me.output_dir = output_dir
         me.filename = filename
@@ -46,15 +51,12 @@ class CassandraCsv(object):
         write_list.append(header)
         for row in result:
             write_list.append(list(row))
-            if max_file_size and\
-             len(write_list) >= max_file_size:
+            if max_file_size and len(write_list) >= max_file_size:
                 me.__write(write_list)
                 write_list = [header]
 
-
         if len(write_list) > 1:
             me.__write(write_list)
-
 
     def __write(self, to_file):
         self.file_pool += 1
@@ -67,12 +69,13 @@ class CassandraCsv(object):
                 pass
 
         if self.max_file_size:
-            _file = os.path.join(_path,
-                                 "%s.csv" % (self.filename % {'partition': self.file_pool} ))
+            _file = os.path.join(
+                _path, "%s.csv" % (self.filename % {"partition": self.file_pool})
+            )
         else:
-            _file = os.path.join(_path,"%s.csv" % self.filename)
+            _file = os.path.join(_path, "%s.csv" % self.filename)
 
-        with open(_file, 'w', newline='', encoding='utf-8') as csvFile:
+        with open(_file, "w", newline="", encoding="utf-8") as csvFile:
             writer = csv.writer(csvFile)
             writer.writerows(to_file)
 
