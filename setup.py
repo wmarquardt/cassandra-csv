e2fc6d84c99c596724f785fb71afd457b4e6825b
William Marquardt <williammqt@gmail.com>
2023-04-03 09:40:35 -0300

    format setup.py

--- setup.py
+++ setup.py
@@ -1,31 +1,28 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='cassandra-csv',
-    version='0.3',
-    packages=find_packages(exclude=['tests*']),
-    license='MIT',
-    description='Export cassandra query result to CSV',
+    name="cassandra-csv",
+    version="0.3",
+    packages=find_packages(exclude=["tests*"]),
+    license="MIT",
+    description="Export cassandra query result to CSV",
     classifiers=[
         # How mature is this project? Common values are
         #   3 - Alpha
         #   4 - Beta
         #   5 - Production/Stable
-        'Development Status :: 3 - Alpha',
-
+        "Development Status :: 3 - Alpha",
         # Indicate who your project is intended for
-        'Intended Audience :: Developers',
-        'Topic :: Software Development :: Libraries :: Python Modules',
-
+        "Intended Audience :: Developers",
+        "Topic :: Software Development :: Libraries :: Python Modules",
         # Pick your license as you wish (should match "license" above)
-        'License :: OSI Approved :: MIT License',
-
+        "License :: OSI Approved :: MIT License",
         # Specify the Python versions you support here. In particular, ensure
         # that you indicate whether you support Python 2, Python 3 or both.
-        'Programming Language :: Python :: 3',
+        "Programming Language :: Python :: 3",
     ],
     install_requires=[],
-    url='https://github.com/wmarquardt/cassandra-csv',
-    author='William Marquardt',
-    author_email='williammqt@gmail.com'
+    url="https://github.com/wmarquardt/cassandra-csv",
+    author="William Marquardt",
+    author_email="williammqt@gmail.com",
 )
